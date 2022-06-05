1538cdbdf135254fce02b1621feb616ef195ca5e
MD Meheddi Hasan Tuhen <tuhenmd28@gmail.com>
2022-06-05 19:06:11 +0600

    complite home page

--- js/main.js
+++ js/main.js
@@ -16,103 +16,27 @@ $('.filterLocation .btnlist>li').click(function(){
     // console.log(jQuery('selector').attr('class').split(' ')[0]);
 })
 
-// menu click dropdown
-$("nav .main-nav__list .nav-item .dropdown-menu>.memutitle").click(function(){
-  
-    $(this).find('ul').toggleClass('show');
-    // $(this).siblings().hide(200);
+$('.top-div .wrap >div').click(function(){
+    $('.top-div').hide(150);
+    $(".panel-fixed").addClass('height')
 })
 
-
-
-
-
-// // for search icon 
-// $(document).ready(function(){
-//     console.log('click');
-//     $("form .formcontent .passenger.sicon li").click(function(){
-//         $(".passenger.sicon .addpsgerwraper").toggle(500);
-//     })
-//     $(".passenger.sicon button.ok.btn.btn-primary").click(function(){
-//         $(".passenger.sicon .addpsgerwraper").hide(500);
-//     })   
-
-// })
-
-// let passengerSicon = $(".passenger.sicon .addpsgerwraper .passengerAdd span");
-// let passengerSicon1 =Number( passengerSicon[0].textContent);
-// let passengerSicon2 = Number(passengerSicon[1].textContent);
-// let passengerSicon3 = Number(passengerSicon[2].textContent);
-// let sumSicon = passengerSicon1 + passengerSicon2 + passengerSicon3;
-// let btnSicon;
-// let countSicon;
-// $("form .formcontent .passenger.sicon .invisible").val(Number(sumSicon.slice(0,1)));
-
-// function additionS(e){
-//     btnSicon = e;
-//     countSicon = Number(btnSicon.previousElementSibling.textContent)
-//     if(countSicon<=10){
-//         ++countSicon;
-//     }
-//     btnSicon.previousElementSibling.innerHTML = countSicon;
-//      passengerSicon1 =Number( passengerSicon[0].textContent);
-//      passengerSicon2 = Number(passengerSicon[1].textContent);
-//      passengerSicon3 = Number(passengerSicon[2].textContent);
-//     sumSicon = passengerSicon1 + passengerSicon2 + passengerSicon3;
-//     if(sumSicon==1){
-//         sumSicon =  `${sumSicon} passenger`;
-//     }else{
-//         sumSicon =  `${sumSicon} passengers`;
-
-//     }
-//     $("form .formcontent .passenger.sicon li samp").text(sumSicon);
-//     $("form .formcontent .passenger.sicon .invisible").val(Number(sumSicon.slice(0,1)));
-
-// }
-// function sumtractS(e){
-//     btnSicon = e;
-//     countSicon = Number(btnSicon.nextElementSibling.textContent)
-//     if(countSicon>=2){
-//         --countSicon;
-//     }
-//     btnSicon.nextElementSibling.innerHTML = countSicon;
-//      passengerSicon1 =Number( passengerSicon[0].textContent);
-//      passengerSicon2 = Number(passengerSicon[1].textContent);
-//      passengerSicon3 = Number(passengerSicon[2].textContent);
-//     sumSicon = passengerSicon1 + passengerSicon2 + passengerSicon3;
-//     if(sumSicon==1){
-//         sumSicon =  `${sumSicon} passenger`;
-//     }else{
-//         sumSicon =  `${sumSicon} passengers`;
-        
-//     }
-//     $("form .formcontent .passenger.sicon li samp").text(sumSicon);
-//     $("form .formcontent .passenger.sicon .invisible").val(Number(sumSicon.slice(0,1)));
-
-// }
-// function sumtract1S(e){
-//     btnSicon = e;
-//     countSicon = Number(btnSicon.nextElementSibling.textContent)
-//     if(countSicon>=1){
-//         --countSicon;
-//     }
-//     btnSicon.nextElementSibling.innerHTML = countSicon;
-//      passengerSicon1 =Number( passengerSicon[0].textContent);
-//      passengerSicon2 = Number(passengerSicon[1].textContent);
-//      passengerSicon3 = Number(passengerSicon[2].textContent);
-//     sumSicon = passengerSicon1 + passengerSicon2 + passengerSicon3;
-//     if(sumSicon==1){
-//         sumSicon =  `${sumSicon} passenger`;
-//     }else{
-//         sumSicon =  `${sumSicon} passengers`;
-
-//     }
-//     $("form .formcontent .passenger.sicon li samp").text(sumSicon);
-//     $("form .formcontent .passenger.sicon .invisible").val(Number(sumSicon.slice(0,1)));
-// }
-// $('html').removeClass('rtl');
- 
-// search icon click 
+$(".lowest>img").click(function(){
+    $('.lowest').hide();
+    $('div#overlay').hide();
+})
+$(".phoneArea .picon img").click(function(){
+    $('.lowest').show();
+    $('div#overlay').show();
+})
+$(".shownumber>img").click(function(){
+    $('.shownumber').hide();
+    $('div#overlay').hide();
+})
+$(".text p").click(function(){
+    $('.shownumber').show();
+    $('div#overlay').show();
+})
 $(".search ").click(function(){
     console.log('click');
     // console.log($('.search-container').hasClass("ami"))
@@ -131,6 +55,8 @@ $(".search ").click(function(){
 })
 function of(){
     $('#overlay').click(function(){
+        $('.lowest').hide();
+        $('.shownumber').hide();
         $(this).hide(100);
         $(".scerch_container").removeClass('show');
         $('.search').children('img').attr('src','img/search.png');
@@ -138,95 +64,91 @@ function of(){
     })
 }
 
+// for search icon 
 $(document).ready(function(){
-    $(".intro__search  form .formcontent .passenger li").click(function(){
-        $(".intro__search .passenger .addpsgerwraper").toggle(500);
+    console.log('click');
+    $("form .formcontent .passenger.sicon li").click(function(){
+        $(".passenger.sicon .addpsgerwraper").toggle(500);
     })
-    $(".intro__search  button.ok.btn.btn-primary").click(function(){
-        $(".intro__search  .passenger .addpsgerwraper").hide(500);
+    $(".passenger.sicon button.ok.btn.btn-primary").click(function(){
+        $(".passenger.sicon .addpsgerwraper").hide(500);
     })   
 
 })
 
-let passenger = $(".intro__search .passenger .addpsgerwraper .passengerAdd span");
-let passenger1 =Number( passenger[0].textContent);
-let passenger2 = Number(passenger[1].textContent);
-let passenger3 = Number(passenger[2].textContent);
-// let passenger34 = Number(passenger[3].html());
-let sum = passenger1 + passenger2 + passenger3;
-let btn;
-let count;
-$(".intro__search form .formcontent .passenger .invisible").val(Number(sum.slice(0,1)));
+let passengerSicon = $(".passenger.sicon .addpsgerwraper .passengerAdd span");
+let passengerSicon1 =Number( passengerSicon[0].textContent);
+let passengerSicon2 = Number(passengerSicon[1].textContent);
+let passengerSicon3 = Number(passengerSicon[2].textContent);
+let sumSicon = passengerSicon1 + passengerSicon2 + passengerSicon3;
+let btnSicon;
+let countSicon;
+$("form .formcontent .passenger.sicon .invisible").val(Number(sumSicon.slice(0,1)));
 
-// console.log(sum);
-function addition(e){
-    btn = e;
-    count = Number(btn.previousElementSibling.textContent)
-    if(count<=10){
-        ++count;
+function additionS(e){
+    btnSicon = e;
+    countSicon = Number(btnSicon.previousElementSibling.textContent)
+    if(countSicon<=10){
+        ++countSicon;
     }
-    btn.previousElementSibling.innerHTML = count;
-     passenger1 =Number( passenger[0].textContent);
-     passenger2 = Number(passenger[1].textContent);
-     passenger3 = Number(passenger[2].textContent);
-    // console.log(passenger1);    
-    sum = passenger1 + passenger2 + passenger3;
-    // console.log(sum);
-    if(sum==1){
-        sum =  `${sum} passenger`;
+    btnSicon.previousElementSibling.innerHTML = countSicon;
+     passengerSicon1 =Number( passengerSicon[0].textContent);
+     passengerSicon2 = Number(passengerSicon[1].textContent);
+     passengerSicon3 = Number(passengerSicon[2].textContent);
+    sumSicon = passengerSicon1 + passengerSicon2 + passengerSicon3;
+    if(sumSicon==1){
+        sumSicon =  `${sumSicon} passenger`;
     }else{
-        sum =  `${sum} passengers`;
+        sumSicon =  `${sumSicon} passengers`;
 
     }
-    $(".intro__search form .formcontent .passenger li samp").text(sum);
-    $(".intro__search form .formcontent .passenger .invisible").val(Number(sum.slice(0,1)));
+    $("form .formcontent .passenger.sicon li samp").text(sumSicon);
+    $("form .formcontent .passenger.sicon .invisible").val(Number(sumSicon.slice(0,1)));
 
 }
-function sumtract(e){
-    btn = e;
-    count = Number(btn.nextElementSibling.textContent)
-    if(count>=2){
-        --count;
+function sumtractS(e){
+    btnSicon = e;
+    countSicon = Number(btnSicon.nextElementSibling.textContent)
+    if(countSicon>=2){
+        --countSicon;
     }
-    btn.nextElementSibling.innerHTML = count;
-     passenger1 =Number( passenger[0].textContent);
-     passenger2 = Number(passenger[1].textContent);
-     passenger3 = Number(passenger[2].textContent);
-    // console.log(passenger1);    
-    sum = passenger1 + passenger2 + passenger3;
-    // console.log(sum);
-    if(sum==1){
-        sum =  `${sum} passenger`;
+    btnSicon.nextElementSibling.innerHTML = countSicon;
+     passengerSicon1 =Number( passengerSicon[0].textContent);
+     passengerSicon2 = Number(passengerSicon[1].textContent);
+     passengerSicon3 = Number(passengerSicon[2].textContent);
+    sumSicon = passengerSicon1 + passengerSicon2 + passengerSicon3;
+    if(sumSicon==1){
+        sumSicon =  `${sumSicon} passenger`;
     }else{
-        sum =  `${sum} passengers`;
+        sumSicon =  `${sumSicon} passengers`;
         
     }
-    $(".intro__search form .formcontent .passenger li samp").text(sum);
-    $(".intro__search form .formcontent .passenger .invisible").val(Number(sum.slice(0,1)));
+    $("form .formcontent .passenger.sicon li samp").text(sumSicon);
+    $("form .formcontent .passenger.sicon .invisible").val(Number(sumSicon.slice(0,1)));
 
 }
-function sumtract1(e){
-    btn = e;
-    count = Number(btn.nextElementSibling.textContent)
-    if(count>=1){
-        --count;
+function sumtract1S(e){
+    btnSicon = e;
+    countSicon = Number(btnSicon.nextElementSibling.textContent)
+    if(countSicon>=1){
+        --countSicon;
     }
-    btn.nextElementSibling.innerHTML = count;
-     passenger1 =Number( passenger[0].textContent);
-     passenger2 = Number(passenger[1].textContent);
-     passenger3 = Number(passenger[2].textContent);
-    // console.log(passenger1);    
-    sum = passenger1 + passenger2 + passenger3;
-    // console.log(sum);
-    if(sum==1){
-        sum =  `${sum} passenger`;
+    btnSicon.nextElementSibling.innerHTML = countSicon;
+     passengerSicon1 =Number( passengerSicon[0].textContent);
+     passengerSicon2 = Number(passengerSicon[1].textContent);
+     passengerSicon3 = Number(passengerSicon[2].textContent);
+    sumSicon = passengerSicon1 + passengerSicon2 + passengerSicon3;
+    if(sumSicon==1){
+        sumSicon =  `${sumSicon} passenger`;
     }else{
-        sum =  `${sum} passengers`;
+        sumSicon =  `${sumSicon} passengers`;
 
     }
-    $(".intro__search form .formcontent .passenger li samp").text(sum);
-    $(".intro__search form .formcontent .passenger .invisible").val(Number(sum.slice(0,1)));
+    $("form .formcontent .passenger.sicon li samp").text(sumSicon);
+    $("form .formcontent .passenger.sicon .invisible").val(Number(sumSicon.slice(0,1)));
 }
+ 
+// search icon click 
 
 
 // slider locatin element filter
